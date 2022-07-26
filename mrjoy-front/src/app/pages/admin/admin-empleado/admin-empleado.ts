79b5333d9f007878c4957cd6ebafc776d33c9069
Jhonatan Segura Galindo <jsegura-e@ripley.com.pe>
2022-07-25 23:40:29 -0500

    ADDED: crear empleado

--- mrjoy-front/src/app/pages/admin/admin-empleado/admin-empleado.ts
+++ mrjoy-front/src/app/pages/admin/admin-empleado/admin-empleado.ts
@@ -1,11 +1,29 @@
-export class Empleado{
-    id_empleado?:number|undefined;
-    nombres:string|undefined;
-    apellidos:string|undefined;
-    telefono: number|undefined;
-    correo:string|undefined;
-    fechaNacimiento:string|undefined;
-    turno:string|undefined;
-    idLogin?:number|undefined;
+export class Empleado {
+    id_empleado?: number | undefined;
+    nombres: string | undefined;
+    apellidos: string | undefined;
+    telefono: number | undefined;
+    correo: string | undefined;
+    fechaNacimiento: string | undefined;
+    turno: string | undefined;
+    idLogin?: number | undefined;
 
+}
+
+export interface IEmpleado {
+    id_empleado?: number;
+    nombres: string;
+    apellidos: string;
+    telefono: string;
+    correo: string;
+    fechaNacimiento: string;
+    turno: string;
+    login: ILogin;
+}
+
+export interface ILogin {
+    id?: number,
+    usuario: string,
+    contrasenia: string,
+    tipouser: string
 }
