0cef66abfafa5eb34af47e21eeae6c06254b2a31
JHONATAN JANSSEN SEGURA GALINDO <jsegura-e@ripley.com.pe>
2023-02-02 17:49:44 -0500

    add: decode token

--- src/app/util/token.util.ts
+++ src/app/util/token.util.ts
@@ -1,4 +1,6 @@
 import { TOKEN } from "../core/constants/constants"
+import jwt_decode from "jwt-decode";
+import { LoginResponse } from "../core/models/response/login.response";
 
 export const setToken = (token: string) => {
   // const payload = JSON.stringify(object)
@@ -6,10 +8,9 @@ export const setToken = (token: string) => {
 }
 
 export const getToken = () => {
-
   const payload = localStorage.getItem(TOKEN)
-  if (payload === null) return ""
+  if (payload === null) return null
 
-  return payload
+  return jwt_decode(payload) as LoginResponse
 
 }
