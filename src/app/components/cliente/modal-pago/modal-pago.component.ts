7ec717ad44ecedc7e3a66b823d200c19c6e65d50
JHONATAN JANSSEN SEGURA GALINDO <jsegurag@ripley.com.pe>
2023-02-27 23:06:30 -0500

    add: try catch modal-pago.component

--- src/app/components/cliente/modal-pago/modal-pago.component.ts
+++ src/app/components/cliente/modal-pago/modal-pago.component.ts
@@ -9,16 +9,15 @@ import { ModalPagoService } from '../../../core/apis/client/modal-pago.service';
   styleUrls: ['./modal-pago.component.scss']
 })
 export class ModalPagoComponent implements OnInit {
-  @Input() datoReservaFormulario : any;
+  @Input() datoReservaFormulario: any;
 
-  numeroTarjeta : String = '';
-  mesTarjeta : String = '';
-  codSeguridad : String = '';
+  numeroTarjeta: String = '';
+  mesTarjeta: String = '';
+  codSeguridad: String = '';
 
-  public validar : boolean = false;
+  public validar: boolean = false;
 
-  constructor(private formularioReservaComponent: FormularioReservaComponent, private modalPagoService: ModalPagoService/*, private modal : NgbModal*/)
-  {
+  constructor(private formularioReservaComponent: FormularioReservaComponent, private modalPagoService: ModalPagoService/*, private modal : NgbModal*/) {
 
   }
 
@@ -27,43 +26,48 @@ export class ModalPagoComponent implements OnInit {
 
   }
 
-  validarPago()
-  {
+  validarPago() {
     this.validar = true;
     console.log('Entro en validar pago ')
+    try {
+      if (this.validar) {
+        if (this.numeroTarjeta.trim() !== '' && this.mesTarjeta.trim() !== '' && this.codSeguridad.trim() !== '') {
+          this.modalPagoService.getTarjeta(this.numeroTarjeta, this.mesTarjeta, this.codSeguridad).subscribe(result => {
+            /*console.log(this.datoReservaFormulario.totalPago)
+            console.log(result.saldo)*/
 
-    if(this.validar)
-    {
-      if (this.numeroTarjeta.trim() !== '' && this.mesTarjeta.trim() !== '' && this.codSeguridad.trim() !== '')
-      {
-        this.modalPagoService.getTarjeta(this.numeroTarjeta, this.mesTarjeta, this.codSeguridad).subscribe( result => {
-          /*console.log(this.datoReservaFormulario.totalPago)
-          console.log(result.saldo)*/
-          if (result)
-          {
-            if (result.saldo >= this.datoReservaFormulario.totalPago)
-            {
-              this.formularioReservaComponent.RegistrarReservaClass(this.datoReservaFormulario)
-              //console.log(result.saldo - this.datoReservaFormulario.totalPago)
-              this.modalPagoService.putTarjeta(this.numeroTarjeta, (result.saldo - this.datoReservaFormulario.totalPago)).subscribe()
+            if (result) {
+              if (result.saldo >= this.datoReservaFormulario.totalPago) {
+                this.formularioReservaComponent.RegistrarReservaClass(this.datoReservaFormulario)
+                //console.log(result.saldo - this.datoReservaFormulario.totalPago)
+                this.modalPagoService.putTarjeta(this.numeroTarjeta, (result.saldo - this.datoReservaFormulario.totalPago)).subscribe()
+
+                alert("Reservacion Completada. Su constancia se le enviara a su correo  en breve.")
+              }
+              else {
+                const audio = new Audio('assets/audios/pipipiii.mp3')
+                audio.volume = 0.4;
+                audio.play();
+                alert('Mano no tienes plata. pipipi ( ͡ಥ ͜ʖ ͡ಥ)')
+              }
+            } else {
+              alert("No se pudo concretar la compra, intentelo denuevo")
             }
-            else
-            {
-              const audio = new Audio('assets/audios/pipipiii.mp3')
-              audio.volume = 0.4;
-              audio.play();
-              alert('Mano no tienes plata. pipipi ( ͡ಥ ͜ʖ ͡ಥ)')
-            }
-          }else{
-            alert("No se pudo concretar la compra, intentelo denuevo")
-          }
-        })
+
+
+
+
+          })
+        }
+        else alert('Datos vacíos')
       }
-      else alert('Datos vacíos')
-    }
-    else
-    {
-      alert('Error Papu :V')
+      else {
+        alert('Error Papu :V')
+      }
+
+
+    } catch (error) {
+      alert("Se se pudo concreta la la accion. Vuelva a intentarlo mas tarde.")
     }
   }
 
