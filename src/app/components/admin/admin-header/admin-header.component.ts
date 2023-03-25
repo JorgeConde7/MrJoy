b8b60899964078ad322f54c9fc41aeca706b7055
JHONATAN JANSSEN SEGURA GALINDO <jsegurag@ripley.com.pe>
2023-03-25 13:04:59 -0500

    add(admin-header): se quito empleados de la vista empleado

--- src/app/components/admin/admin-header/admin-header.component.ts
+++ src/app/components/admin/admin-header/admin-header.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { getPayload } from 'src/app/util/token.util';
 
 @Component({
   selector: 'app-admin-header',
@@ -8,17 +9,25 @@ import { Router } from '@angular/router';
 })
 export class AdminHeaderComponent implements OnInit {
 
-  
-  constructor(private router:Router) { }
+  sesionData = {
+    isAdmin: false
+  }
+
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
-    
+
   }
 
   isUrlEqualTo(currentURL: string) {
     const url = this.router.url
 
     return url === currentURL;
+  }
+
+  setSesionData() {
+    const sesionData = getPayload()!
+    this.sesionData.isAdmin = sesionData.profile === "admin"
   }
 
 }
