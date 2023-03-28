b19c450f03441498ca40800c02454e65b83f2ac6
JHONATAN JANSSEN SEGURA GALINDO <jsegurag@ripley.com.pe>
2023-03-28 00:17:47 -0500

    add(enviroments.prod): Se agrego url mr joi

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
   //API_URL_BASE: "https://rickandmortyapi.com/api/",
-  URL_BASE: "http://localhost:8090",
+  URL_BASE: "backend-mrjoi.fly.dev",
   DATATABLE_LANGUAJE: "https://cdn.datatables.net/plug-ins/1.12.1/i18n/es-MX.json"
 };
