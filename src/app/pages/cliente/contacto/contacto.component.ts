c2474ea4736498b1f0b77dbe8092d11e14125dff
JHONATAN JANSSEN SEGURA GALINDO <jsegurag@ripley.com.pe>
2023-04-04 14:50:57 -0500

    add(contacto.component): Se agregó sweet alert al registrar

--- src/app/pages/cliente/contacto/contacto.component.ts
+++ src/app/pages/cliente/contacto/contacto.component.ts
@@ -2,16 +2,15 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { ContactoService } from 'src/app/core/apis/client/contacto.service';
 import { Contacto } from 'src/app/core/models/client/contacto';
+import { alertNotification } from 'src/app/util/notifications';
 import { getPayload } from 'src/app/util/token.util';
-
 
 @Component({
   selector: 'app-contacto',
   templateUrl: './contacto.component.html',
-  styleUrls: ['./contacto.component.scss']
+  styleUrls: ['./contacto.component.scss'],
 })
 export class ContactoComponent implements OnInit {
-
   nombres!: string;
   correo!: string;
   telefono!: string;
@@ -25,29 +24,40 @@ export class ContactoComponent implements OnInit {
   constructor(
     private contactoService: ContactoService,
     private router: Router
-  ) { this.setDefaultValues() }
-
-  ngOnInit(): void {
-
+  ) {
+    this.setDefaultValues();
   }
 
-  setDefaultValues() {
-    const payLoad = getPayload()
-    this.nombres = payLoad?.nombres ? payLoad.nombres : ""
-    this.correo = payLoad?.correo ? payLoad.correo : "" ;
-    this.telefono = payLoad?.telefono ? payLoad.telefono : "";
-    this.idLogin = Number(payLoad?.id ? payLoad.id : "");
+  ngOnInit(): void {}
 
+  setDefaultValues() {
+    const payLoad = getPayload();
+    this.nombres = payLoad?.nombres ? payLoad.nombres : '';
+    this.correo = payLoad?.correo ? payLoad.correo : '';
+    this.telefono = payLoad?.telefono ? payLoad.telefono : '';
+    this.idLogin = Number(payLoad?.id ? payLoad.id : '');
   }
 
   onCreate(): void {
-    const contacto = new Contacto(this.nombres, this.correo, this.telefono, this.asunto, 
-      this.estado, this.descripcion, this.fechaRegistro, this.idLogin);
-    this.contactoService.create(contacto).subscribe(
-      _ => {
-        this.router.navigate([""])
-      }
-    )
+    const contacto = new Contacto(
+      this.nombres,
+      this.correo,
+      this.telefono,
+      this.asunto,
+      this.estado,
+      this.descripcion,
+      this.fechaRegistro,
+      this.idLogin
+    );
+
+    this.contactoService.create(contacto).subscribe((_) => {
+      alertNotification('', 'Consulta enviada', 'success');
+      this.cleanForm();
+    });
   }
 
+  cleanForm() {
+    this.asunto = '';
+    this.descripcion = '';
+  }
 }
